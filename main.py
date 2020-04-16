7e62aad5e3db84195b59cd19bda5caae6761218c
Wissem <wissem.derghal@gmail.com>
2020-04-16 15:24:04 +0200

    config_2 (not finished)

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 # coding: utf-8
 import os
 
-array = ["","","","","","","","",""]
+array = [[''] * 3 for i in range(0,3)]
 
-print(array)
-
+print(array[0])
+print(array[1])
+print(array[2])
 
 def saisieCoords(joueur):
-    CoordsX = input("Choisissez la case en x : ")
-    CoordsY = input("Choisissez la case en y : ")
+    CoordsX = int(input("Choisissez la case en x : "))
+    CoordsY = int(input("Choisissez la case en y : "))
     
     if joueur == 1:
-        print("Joueur 1 à toi de jouer !")
-        array[CoordsX] = "x"
-        array[CoordsY] = "x"
+        if CoordsX == "x" and CoordsY == "x":
+            print("La case a déja été choisie !")
+            pass           
+        else:
+            print("Joueur 1 à toi de jouer !")
+            array[CoordsY-1][CoordsX-1] = 'x'
     elif joueur == 2:
-        print("Joueur 2 à toi de jouer !")
-        array[CoordsX] = "o"
-        array[CoordsY] = "o"
+        if CoordsX == "o" and CoordsY == "o":
+            print("La case a déja été choisie !")
+            pass
+        else:
+            print("Joueur 2 à toi de jouer !")
+            array[CoordsY-1][CoordsX-1] = 'o'
     
 
 continuer = 1
@@ -27,11 +34,17 @@ while continuer == 1:
     if joueur == 1:
         print("C'est au joueur 1 de jouer !")
         saisieCoords(1)
-        print(grille)
+        print(array[0])
+        print(array[1])
+        print(array[2])
         joueur = 2
     elif joueur == 2:
         print("C'est au joueur 2 de jouer !")
         saisieCoords(2)
-        print(grille)
+        print(array[0])
+        print(array[1])
+        print(array[2])
+        joueur = 1
+    
 
 os.system("pause")
