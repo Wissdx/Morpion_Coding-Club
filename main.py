27b6e33ed753cca258afa30474e837f93e6824b5
Wissem <wissem.derghal@gmail.com>
2020-04-16 14:08:22 +0200

    config_1

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+
+
+array1 = ["","",""]
+array2 = ["","",""]
+array3 = ["","",""]
+
+print(array1)
+print(array2)
+print(array3)
+
+joueur1 = ""
+joueur2 = ""
+
+def saisieCoords():
+    CoordsX = input("Choisissez la case en x : ")
+    CoordsY = input("Choisissez la case en y : ")
+    
+    
+    
+
+continuer = 1
+
+while continuer == 1:
+    if 
+
+os.system("pause")
